2752c6981adba6177e75d70f77626646951dcad4
a1pl <157911417+a1pl@users.noreply.github.com>
2026-06-24 16:58:26 -0400

    remove starting window

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { For, createSignal, type Component } from 'solid-js';
 import Window from './Window';
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol essentially just uncollidable uuid-like 
 const App: Component = () => {
-  const [windows, setWindows] = createSignal<symbol[]>([Symbol()]);
+  const [windows, setWindows] = createSignal<symbol[]>([]);
 
   const spawn = () => setWindows(windows().concat(Symbol()));
   const closeWindow = (id: symbol) => setWindows(windows().filter(w => w !== id));
