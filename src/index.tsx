65272fda79d6901c17baf208e72e63b704f7cc8f
a1pl <157911417+a1pl@users.noreply.github.com>
2026-06-30 15:07:40 -0400

    make stuff usable

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,42 @@ import { render } from "solid-js/web";
 import "solid-devtools";
 
 import App from "./Core/App";
+import { WindowHandle } from "./Apis/iSApi";
+import * as systems from "./Core/systems";
+import * as registryApi from "./Apis/RegistryApi";
+import * as fileSystemApi from "./Apis/FileSystemApi";
+
+declare global {
+  interface Window {
+    __API: {
+      WindowHandle: typeof WindowHandle;
+      systems: typeof systems;
+      registry: typeof registryApi;
+      fs: typeof fileSystemApi;
+      version: string;
+    };
+    WindowHandle: typeof WindowHandle;
+  }
+}
+
+const API = {
+  WindowHandle, // hog off windowhandle
+  systems,
+  registry: registryApi,
+  fs: fileSystemApi,
+  version: "1.0.0",
+};
+
+// make global
+window.__API = API;
+window.WindowHandle = WindowHandle;
+
+// logging
+if (import.meta.env.DEV) {
+  console.log("available:", Object.keys(API));
+  console.log("WindowHandle:", WindowHandle);
+}
+
 
 const root = document.getElementById("root");
 
